e6ac4e1d6bdc8a0bd763f9cc07930b5379013b1f
Emily Creamer <emily.creamer@icloud.com>
2019-05-29 14:19:03 -0700

    Middleware with notes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
+// Store is the redux store
+// Next is a function to move to the next middleware
+// Action is the dispatched action
+
+const logger = store => next => action => {
+  // Before the reducer has changed state
+  console.log('Before reducer.', store.getState());
+
+  next(action); // Go to the next middleware
+
+  // After the reducer has changed state
+  console.log('After the reducer', store.getState());
+};
+
+const logger2 = store => next => action => {
+  console.log('I am another Middleware.', action);
+  next(action);
+};
+
+function reducer(state = {}, action) {
+  switch(action.type) {
+    case 'Hello.':
+      return 'Hi...';
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  reducer,
+  // applyMiddleware is like app.use
+  applyMiddleware(logger, logger2)
 );
+
+store.dispatch({
+  type: 'Hello.'
+});
