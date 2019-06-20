51c68075be0893320ce7db46435984d130c92190
Emily Creamer <emily.creamer@icloud.com>
2019-06-20 14:03:23 -0700

    Provider wrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
+import store from './store';
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
